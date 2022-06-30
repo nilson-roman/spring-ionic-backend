422ab76a9519363273d57e6e950261a095aa6bb6
Nilson Roman <nilsonoroman@gmail.com>
2022-06-29 22:19:13 -0300

    Creating repository and service for Categoria

--- src/main/java/com/udemy/nelio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/nelio/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,10 @@
 package com.udemy.nelio.cursomc.resources;
 
 import com.udemy.nelio.cursomc.domain.Categoria;
+import com.udemy.nelio.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +16,12 @@ import java.util.List;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar(){
-        Categoria cat1 = new Categoria(1,"Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
+    @Autowired
+    private CategoriaService service;
 
-        List<Categoria> list = new ArrayList<>();
-        list.add(cat1);
-        list.add(cat2);
-
-        return list;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
