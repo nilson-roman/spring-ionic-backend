953b09b851c8145851d929a504fac6603fc2206f
Nilson Roman <nilsonoroman@gmail.com>
2022-06-30 22:54:54 -0300

    Creating instantiation operation

--- src/main/java/com/udemy/nelio/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/nelio/cursomc/CursomcApplication.java
@@ -1,13 +1,28 @@
 package com.udemy.nelio.cursomc;
 
+import com.udemy.nelio.cursomc.domain.Categoria;
+import com.udemy.nelio.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
