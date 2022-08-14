1e16aa3c2f23e58283bdb7d8d744a9124a8fec0c
Nilson Roman <nilsonoroman@gmail.com>
2022-08-14 20:24:22 -0300

    Customizing the validation return

--- src/main/java/com/udemy/nelio/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/nelio/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.udemy.nelio.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +27,19 @@ public class ResourceExceptionHandler {
                                                         HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(),
                 e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e,
+                                                       HttpServletRequest request) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),
+                "Error de validação", System.currentTimeMillis());
+
+        for(FieldError error : e.getBindingResult().getFieldErrors()){
+           err.addError(error.getField(), error.getDefaultMessage());
+        }
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
