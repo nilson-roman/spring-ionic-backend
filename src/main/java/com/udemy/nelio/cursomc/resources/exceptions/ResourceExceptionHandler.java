fd6809d6caa26da4eff56793a3f2222366ee20e4
Nilson Roman <nilsonoroman@gmail.com>
2022-08-14 11:15:55 -0300

    Deleting a Categoria with DELETE

--- src/main/java/com/udemy/nelio/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/nelio/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.udemy.nelio.cursomc.resources.exceptions;
 
+import com.udemy.nelio.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,13 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),
                 e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e,
+                                                        HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/udemy/nelio/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/nelio/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.udemy.nelio.cursomc.services;
 
 import com.udemy.nelio.cursomc.domain.Categoria;
 import com.udemy.nelio.cursomc.repositories.CategoriaRepository;
+import com.udemy.nelio.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,5 +30,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj){
         find(obj.getId());
         return repository.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 }
