bd3c761b577b34781ee4566ba41893b6ec0116f1
Nilson Roman <nilsonoroman@gmail.com>
2022-08-15 21:35:38 -0300

    Inserting new Cliente with POST

--- src/main/java/com/udemy/nelio/cursomc/resources/ClienteResource.java
+++ src/main/java/com/udemy/nelio/cursomc/resources/ClienteResource.java
@@ -2,13 +2,16 @@ package com.udemy.nelio.cursomc.resources;
 
 import com.udemy.nelio.cursomc.domain.Cliente;
 import com.udemy.nelio.cursomc.dto.ClienteDTO;
+import com.udemy.nelio.cursomc.dto.ClienteNewDTO;
 import com.udemy.nelio.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,6 +26,15 @@ public class ClienteResource {
     public ResponseEntity<Cliente> find(@PathVariable Integer id) {
         Cliente obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+        Cliente obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/udemy/nelio/cursomc/services/ClienteService.java
+++ src/main/java/com/udemy/nelio/cursomc/services/ClienteService.java
@@ -1,16 +1,22 @@
 package com.udemy.nelio.cursomc.services;
 
+import com.udemy.nelio.cursomc.domain.Cidade;
 import com.udemy.nelio.cursomc.domain.Cliente;
+import com.udemy.nelio.cursomc.domain.Endereco;
+import com.udemy.nelio.cursomc.domain.enums.TipoCliente;
 import com.udemy.nelio.cursomc.dto.ClienteDTO;
+import com.udemy.nelio.cursomc.dto.ClienteNewDTO;
 import com.udemy.nelio.cursomc.repositories.ClienteRepository;
+import com.udemy.nelio.cursomc.repositories.EnderecoRepository;
 import com.udemy.nelio.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,46 +25,70 @@ import java.util.Optional;
 public class ClienteService {
 
     @Autowired
-    private ClienteRepository repository;
+    private ClienteRepository repo;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
     public Cliente find(Integer id) {
-        Optional<Cliente> obj = repository.findById(id);
+        Optional<Cliente> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
-                "Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+    @Transactional
+    public Cliente insert(Cliente obj) {
+        obj.setId(null);
+        obj = repo.save(obj);
+        enderecoRepository.saveAll(obj.getEnderecos());
+        return obj;
     }
 
     public Cliente update(Cliente obj) {
         Cliente newObj = find(obj.getId());
         updateData(newObj, obj);
-        return repository.save(newObj);
+        return repo.save(newObj);
     }
 
     public void delete(Integer id) {
         find(id);
         try {
-            repository.deleteById(id);
+            repo.deleteById(id);
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
         }
     }
 
     public List<Cliente> findAll() {
-        return repository.findAll();
+        return repo.findAll();
     }
 
     public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-        PageRequest pageRequest = PageRequest
-                .of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-        return repository.findAll(pageRequest);
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
     }
 
-    public Cliente fromDTO(ClienteDTO dto) {
-        return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
     }
 
-    private Cliente updateData(Cliente newObj, Cliente obj) {
+    public Cliente fromDTO(ClienteNewDTO objDto) {
+        Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+        Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+        Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+        cli.getEnderecos().add(end);
+        cli.getTelefones().add(objDto.getTelefone1());
+        if (objDto.getTelefone2() != null) {
+            cli.getTelefones().add(objDto.getTelefone2());
+        }
+        if (objDto.getTelefone3() != null) {
+            cli.getTelefones().add(objDto.getTelefone3());
+        }
+        return cli;
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
         newObj.setNome(obj.getNome());
         newObj.setEmail(obj.getEmail());
-        return newObj;
     }
 }
