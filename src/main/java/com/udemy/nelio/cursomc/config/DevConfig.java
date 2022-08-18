a33491d442d7bf623d6de7a3dd7292941a6cfd86
Nilson Roman <nilsonoroman@gmail.com>
2022-08-18 10:21:34 -0300

    Creating the development profile

---
+++ src/main/java/com/udemy/nelio/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.udemy.nelio.cursomc.config;
+
+import com.udemy.nelio.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String stratagy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        if(!"create".equals(stratagy)){
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
