d09455d525a7e0c513df886c5cb55a86c224e02c
Nilson Roman <nilsonoroman@gmail.com>
2022-08-20 19:11:47 -0300

    Saving user profiles in the database

--- src/main/java/com/udemy/nelio/cursomc/domain/Cliente.java
+++ src/main/java/com/udemy/nelio/cursomc/domain/Cliente.java
@@ -1,11 +1,13 @@
 package com.udemy.nelio.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.udemy.nelio.cursomc.domain.enums.Perfil;
 import com.udemy.nelio.cursomc.domain.enums.TipoCliente;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente implements Serializable {
@@ -28,11 +30,16 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @ElementCollection(fetch=FetchType.EAGER)
+    @CollectionTable(name="PERFIS")
+    private Set<Integer> perfis = new HashSet<>();
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
     public Cliente() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -42,6 +49,7 @@ public class Cliente implements Serializable {
         this.cpfOuCnpj = cpfOuCnpj;
         this.tipo = (tipo == null) ? null: tipo.getCod();
         this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Integer getId() {
@@ -90,6 +98,14 @@ public class Cliente implements Serializable {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil.getCod());
     }
 
     public List<Endereco> getEnderecos() {
