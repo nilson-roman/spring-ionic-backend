b7895de3393583fcfe667537ad4382305a71d06f
Nilson Roman <nilsonoroman@gmail.com>
2022-08-20 21:13:23 -0300

    Implementing authorization

--- src/main/java/com/udemy/nelio/cursomc/config/SecurityConfig.java
+++ src/main/java/com/udemy/nelio/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.udemy.nelio.cursomc.config;
 
 import com.udemy.nelio.cursomc.security.JWTAuthenticationFilter;
+import com.udemy.nelio.cursomc.security.JWTAuthorizationFilter;
 import com.udemy.nelio.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
