623934ba1705af32d0dfae9101956e4d37550dd7
Nilson Roman <nilsonoroman@gmail.com>
2022-08-20 22:05:59 -0300

    Content restriction: Cliente only retrieves their orders

--- src/main/java/com/udemy/nelio/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/udemy/nelio/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,16 @@
 package com.udemy.nelio.cursomc.repositories;
 
+import com.udemy.nelio.cursomc.domain.Cliente;
 import com.udemy.nelio.cursomc.domain.Pedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    @Transactional(readOnly=true)
+    Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/udemy/nelio/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/nelio/cursomc/services/PedidoService.java
@@ -1,5 +1,6 @@
 package com.udemy.nelio.cursomc.services;
 
+import com.udemy.nelio.cursomc.domain.Cliente;
 import com.udemy.nelio.cursomc.domain.ItemPedido;
 import com.udemy.nelio.cursomc.domain.PagamentoComBoleto;
 import com.udemy.nelio.cursomc.domain.Pedido;
@@ -7,12 +8,18 @@ import com.udemy.nelio.cursomc.domain.enums.EstadoPagamento;
 import com.udemy.nelio.cursomc.repositories.ItemPedidoRepository;
 import com.udemy.nelio.cursomc.repositories.PagamentoRepository;
 import com.udemy.nelio.cursomc.repositories.PedidoRepository;
+import com.udemy.nelio.cursomc.security.UserSS;
+import com.udemy.nelio.cursomc.services.exceptions.AuthorizationException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Optional;
+
+import static org.springframework.data.domain.Sort.Direction;
 
 @Service
 public class PedidoService {
@@ -65,5 +72,15 @@ public class PedidoService {
         itemPedidoRepository.saveAll(obj.getItens());
         emailService.sendOrderConfirmationHtmlEmail(obj);
         return obj;
+    }
+
+    public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        UserSS user = UserService.authenticated();
+        if (user == null) {
+            throw new AuthorizationException("Acesso negado");
+        }
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Cliente cliente =  clienteService.find(user.getId());
+        return repository.findByCliente(cliente, pageRequest);
     }
 }
