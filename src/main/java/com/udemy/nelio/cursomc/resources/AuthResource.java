715d055631ddc2c2ca49bc1db6f0b6166dda04ec
Nilson Roman <nilsonoroman@gmail.com>
2022-08-20 22:26:49 -0300

    Forgot the password

--- src/main/java/com/udemy/nelio/cursomc/resources/AuthResource.java
+++ src/main/java/com/udemy/nelio/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.udemy.nelio.cursomc.resources;
 
+import com.udemy.nelio.cursomc.dto.EmailDTO;
 import com.udemy.nelio.cursomc.security.JWTUtil;
 import com.udemy.nelio.cursomc.security.UserSS;
+import com.udemy.nelio.cursomc.services.AuthService;
 import com.udemy.nelio.cursomc.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -18,11 +22,20 @@ public class AuthResource {
     @Autowired
     private JWTUtil jwtUtil;
 
+    @Autowired
+    private AuthService service;
+
     @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
     public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
         UserSS user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/forgot", method = RequestMethod.POST)
+    public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+        service.sendNewPassword(objDto.getEmail());
         return ResponseEntity.noContent().build();
     }
 }
