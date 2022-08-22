23f085ceb79c5485d9904d683ee751c7b3f507a8
Nilson Roman <nilsonoroman@gmail.com>
2022-08-22 20:48:09 -0300

    Using the naming pattern for images

--- src/main/java/com/udemy/nelio/cursomc/services/ClienteService.java
+++ src/main/java/com/udemy/nelio/cursomc/services/ClienteService.java
@@ -14,6 +14,7 @@ import com.udemy.nelio.cursomc.services.exceptions.AuthorizationException;
 import com.udemy.nelio.cursomc.services.exceptions.DataIntegrityException;
 import com.udemy.nelio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -41,6 +43,12 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id) {
 
@@ -116,12 +124,9 @@ public class ClienteService {
             throw new AuthorizationException("Acesso negado");
         }
 
-        URI uri = s3Service.uploadFile(multipartFile);
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + user.getId() + ".jpg";
 
-        Cliente cli = find(user.getId());
-        cli.setImgeUrl(uri.toString());
-        repo.save(cli);
-
-        return uri;
+        return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
 }
