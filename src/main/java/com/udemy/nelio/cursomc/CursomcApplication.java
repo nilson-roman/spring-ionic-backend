98fc0c5b9ccadb2c052cd16b9394accc78212d0a
Nilson Roman <nilsonoroman@gmail.com>
2022-08-21 21:01:20 -0300

    Saving first file on S3

--- src/main/java/com/udemy/nelio/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/nelio/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.udemy.nelio.cursomc;
 
+import com.udemy.nelio.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/home/nilson/Downloads/fender.jpg");
 	}
 }
